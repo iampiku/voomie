ad3fec2b83dfaae36e8b68a8a5235d77218c80d5
Pradipta Chatterjee <iampikuchatterjee@gmail.com>
2022-08-11 23:12:03 +0530

    🏪 implementing vuex store

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,72 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import MovieService from '@/service/MovieService';
+const movieService = new MovieService();
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		popularMovies: [],
+		moviesInTheater: [],
+		upComingMovies: [],
+		latestMovies: {},
+	},
+	getters: {
+		getPopularMovies(state) {
+			return state.popularMovies;
+		},
+
+		getMoviesInTheater(state) {
+			return state.moviesInTheater;
+		},
+
+		getUpComingMovies(state) {
+			return state.upComingMovies;
+		},
+
+		getLatestMovies(state) {
+			return state.latestMovies;
+		},
+	},
+	mutations: {
+		setPopularMovies(state, popularMovies) {
+			state.popularMovies = popularMovies;
+		},
+
+		setMoviesInTheater(state, moviesInTheater) {
+			state.moviesInTheater = moviesInTheater;
+		},
+
+		setUpComingMovies(state, upComingMovies) {
+			state.upComingMovies = upComingMovies;
+		},
+
+		setLatestMovies(state, latestMovies) {
+			state.latestMovies = latestMovies;
+		},
+	},
+	actions: {
+		async popularMovies({ commit }) {
+			const data = await movieService.getPopularMovies();
+			commit('setPopularMovies', data);
+		},
+
+		async moviesInTheater({ commit }) {
+			const data = await movieService.getMoviesInTheater();
+			commit('setMoviesInTheater', data);
+		},
+
+		async upComingMovies({ commit }) {
+			const data = await movieService.getUpcomingMovies();
+			commit('setUpComingMovies', data);
+		},
+
+		async latestMovies({ commit }) {
+			const data = await movieService.getLatestMovie();
+			commit('setLatestMovies', data);
+		},
+	},
+	modules: {},
+});
