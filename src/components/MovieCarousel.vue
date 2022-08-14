4d9c2e498590ae9f12b9233677c5e09758aa4f31
Pradipta Chatterjee <iampikuchatterjee@gmail.com>
2022-08-15 02:11:11 +0530

    implementing carousel

--- src/components/MovieCarousel.vue
+++ src/components/MovieCarousel.vue
@@ -1,3 +1,44 @@
-<template></template>
+<template>
+	<v-carousel
+		cycle
+		height="350"
+		next-icon="mdi-menu-right"
+		prev-icon="mdi-menu-left"
+		show-arrows-on-hover
+		hide-delimiters
+		hide-delimiter-background
+	>
+		<v-carousel-item
+			v-for="(movie, index) in movies"
+			:key="index"
+			@click="$emit('onClick', movie.id)"
+		>
+			<v-card
+				ripple
+				height="350"
+				:img="`http://image.tmdb.org/t/p/original${movie.backdrop_path}`"
+			>
+				<v-card-title class="font-weight-bold white--text">{{
+					movie.title
+				}}</v-card-title>
+				<v-card-text class="font-weight-regular text--primary">{{
+					movie.overview
+				}}</v-card-text>
+			</v-card>
+		</v-carousel-item>
+	</v-carousel>
+</template>
 
-<script></script>
+<script>
+	export default {
+		name: 'movie-carousel',
+		data: () => ({}),
+
+		props: {
+			movies: {
+				type: [],
+				required: true,
+			},
+		},
+	};
+</script>
