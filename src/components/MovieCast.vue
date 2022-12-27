d431fda6751ea3cb7475807460f20772d8b41221
iampiku <iampikuchatterjee@gmail.com>
2022-12-27 13:03:54 +0530

    😎 movie cast chip component added.

---
+++ src/components/MovieCast.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-chip>
+		<v-avatar v-if="cast.profile_path">
+			<v-img :src="`http://image.tmdb.org/t/p/original${cast.profile_path}`">
+			</v-img>
+		</v-avatar>
+		<span class="mx-2">{{ cast.original_name }}</span>
+		<span v-if="cast.character">Character: {{ cast.character }}</span>
+	</v-chip>
+</template>
+
+<script>
+	export default {
+		name: 'MovieCast',
+
+		props: {
+			cast: {
+				type: Object,
+				required: true,
+			},
+		},
+	};
+</script>
